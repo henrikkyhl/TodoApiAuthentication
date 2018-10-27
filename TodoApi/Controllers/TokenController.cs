69db04a51b66124c0043948dae83e8efb6dde226
Henrik Kühl <henrikkyhl@hotmail.com>
2018-10-27 12:38:27 +0200

    Password hashing and salting implemented

--- TodoApi/Controllers/TokenController.cs
+++ TodoApi/Controllers/TokenController.cs
@@ -32,14 +32,34 @@ namespace TodoApi.Controllers
                 return Unauthorized();
 
             // check if password is correct
-            if (!model.Password.Equals(user.Password))
+            if (!VerifyPasswordHash(model.Password, user.PasswordHash, user.PasswordSalt))
                 return Unauthorized();
 
             // Authentication successful
-            return Ok(new {
+            return Ok(new
+            {
                 username = user.Username,
                 token = GenerateToken(user)
             });
+        }
+
+        // This method verifies that the password entered by a user corresponds to the stored
+        // password hash for this user. The method computes a Hash-based Message Authentication
+        // Code (HMAC) using the SHA512 hash function. The inputs to the computation is the
+        // password entered by the user and the stored password salt for this user. If the
+        // computed hash value is identical to the stored password hash, the password entered
+        // by the user is correct, and the method returns true.
+        private bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != storedHash[i]) return false;
+                }
+            }
+            return true;
         }
 
         // This method generates and returns a JWT token for a user.
@@ -61,7 +81,7 @@ namespace TodoApi.Controllers
                                null, // audience - not needed (ValidateAudience = false)
                                claims.ToArray(), 
                                DateTime.Now,               // notBefore
-                               DateTime.Now.AddDays(1)));  // expires
+                               DateTime.Now.AddMinutes(10)));  // expires
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
--- TodoApi/Data/DbInitializer.cs
+++ TodoApi/Data/DbInitializer.cs
@@ -26,19 +26,24 @@ namespace TodoApi.Data
                 new TodoItem { IsComplete=false, Name="Sleep"}
             };
 
-            // Create two users
+            // Create two users with hashed and salted passwords
             string password = "1234";
+            byte[] passwordHashJoe, passwordSaltJoe, passwordHashAnn, passwordSaltAnn;
+            CreatePasswordHash(password, out passwordHashJoe, out passwordSaltJoe);
+            CreatePasswordHash(password, out passwordHashAnn, out passwordSaltAnn);
 
             List<User> users = new List<User>
             {
                 new User {
                     Username = "UserJoe",
-                    Password = password,
+                    PasswordHash = passwordHashJoe,
+                    PasswordSalt = passwordSaltJoe,
                     IsAdmin = false
                 },
                 new User {
                     Username = "AdminAnn",
-                    Password = password,
+                    PasswordHash = passwordHashAnn,
+                    PasswordSalt = passwordSaltAnn,
                     IsAdmin = true
                 }
             };
@@ -47,6 +52,28 @@ namespace TodoApi.Data
             context.Users.AddRange(users);
             context.SaveChanges();
 
+        }
+
+        // This method computes a hashed and salted password using the HMACSHA512 algorithm.
+        // The HMACSHA512 class computes a Hash-based Message Authentication Code (HMAC) using 
+        // the SHA512 hash function. When instantiated with the parameterless constructor (as
+        // here) a randomly Key is generated. This key is used as a password salt.
+
+        // The computation is performed as shown below:
+        //   passwordHash = SHA512(password + Key)
+
+        // A password salt randomizes the password hash so that two identical passwords will
+        // have significantly different hash values. This protects against sophisticated attempts
+        // to guess passwords, such as a rainbow table attack.
+        // The password hash is 512 bits (=64 bytes) long.
+        // The password salt is 1024 bits (=128 bytes) long.
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
 
     }
