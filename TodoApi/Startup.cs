63eca81b1273176565f7b30e60e46a4c621ecf2a
Henrik Kühl <henrikkyhl@hotmail.com>
2019-10-18 13:46:36 +0200

    Updated to .NET Core 2.2

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -83,7 +83,7 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // Initialize the database and the AuthenticationHelper.
+            // Initialize the database.
             using (var scope = app.ApplicationServices.CreateScope())
             {
                 // Initialize the database
